d1271c559017d8ed080c86a8e954dd2aa1015a1e
Kelvin Liang <kelvin9877@gmail.com>
2020-01-17 09:46:35 +0800

    finish array-analysis

--- index.js
+++ index.js
@@ -59,9 +59,25 @@ const caesarCipher = (() => {
 
 })();
 
+const arrayAnalysis = (array) => {
+
+  const getAvg = arr => arr.reduce((sum, num) => sum + num, 0) / arr.length;
+  const getMin = arr => Math.min(...arr);
+  const getMax = arr => Math.max(...arr);
+
+  return {
+    average: getAvg(array),
+    min: getMin(array),
+    max: getMax(array),
+    length: array.length
+  };
+
+};
+
 export {
   capitalize,
   reverse,
   Calculator,
-  caesarCipher
+  caesarCipher,
+  arrayAnalysis
 };
--- index.test.js
+++ index.test.js
@@ -3,6 +3,7 @@ import {
   reverse,
   Calculator,
   caesarCipher,
+  arrayAnalysis,
 }
   from './index';
 
@@ -57,5 +58,12 @@ test('Caesar Cipher decrypt', () => {
 
   expect(caesarCipher.decrypt('ohhoqy oh rokb - Ufsoh Dzob!', 14))
     .toBe('attack at dawn - Great Plan!');
+});
+
+test('Array Analysis', () => {
+  expect(arrayAnalysis([1, 2, 5, 8, 9]))
+    .toStrictEqual({ average: 5, min: 1, max: 9, length: 5 });
+  expect(arrayAnalysis([21, 98, 23, 20, -13, -3, 0, 182, 31, 78]))
+    .toStrictEqual({ average: 43.7, min: -13, max: 182, length: 10 });
 });
 
